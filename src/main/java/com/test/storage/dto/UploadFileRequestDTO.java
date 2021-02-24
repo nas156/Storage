27484e8d335faa70570b7b572fe85316ffaf1c31
nas156 <31134655+nas156@users.noreply.github.com>
2021-02-24 19:38:22 +0200

    add upload file name regex validation

--- src/main/java/com/test/storage/dto/UploadFileRequestDTO.java
+++ src/main/java/com/test/storage/dto/UploadFileRequestDTO.java
@@ -5,11 +5,13 @@ import lombok.Data;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
 public class UploadFileRequestDTO {
 
+    @Pattern(regexp = "^[\\w]+[\\w|\\s-]*\\.[A-Za-z0-9]+$", message = "name of file must be like name.ext")
     @NotEmpty(message = "name of file must not be empty")
     private String name;
 
